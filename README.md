a0342a0c59514d00b730698a7a678a27bd1d0ab2
Snugglez <32309975+Snugglez@users.noreply.github.com>
2022-06-04 20:20:18 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # emote-prediction
+
 Removes ping tax from emotes with a configurable delay for sit/stand to emulate any ping teabag if you use an insert macro
+
+# Commands
+Note, if using the commands in the toolbox channel or `/8`, ignore the `!` prefix
+
+Command | Argument(s) | Example | Description
+---|---|---|---
+**!emote** | NONE | !emote| Returns commands and their values
+**!emote** | idle | !emote idle| Blocks or allows idle emotes (blocked by default)
+**!emote** | delay # | !emote delay 30| Sets the delay in ms for sit/stand to emulate teabag at different ping properly (30 by default)
+
+note, if you do not care about teabagging, you can set delay to 0 to have zero ping tax on sitting or standing
---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = function reee(d) {
+    d.command.add('emote', (arg, arg2) => {
+        if (arg == 'delay') {
+            d.settings.delay = arg2 * 1
+            d.command.message(`sit delay set to ${arg2}`)
+        }
+        if (arg == 'idle') {
+            d.settings.idles = !d.settings.idles
+            d.command.message(`idle animations ${d.settings.idles ? 'shown' : 'blocked'}`)
+        }
+    })
+
+    d.hook('C_SOCIAL', '*', (e) => {
+        if ([38, 39].includes(e.emote)) setTimeout(() => { cSocial(e) }, d.settings.delay)
+        else cSocial(e)
+    })
+
+    d.hook('S_SOCIAL', '*', (e) => {
+        if (!snug.me.is(e.target)) return
+        if (d.settings.idles && [31, 32, 33].includes(e.animation)) return
+        return false
+    })
+
+    function cSocial(e) { d.send('S_SOCIAL', '*', { target: snug.me.gameId, animation: e.emote }) }
+}
